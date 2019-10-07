be54bf62fcd8f58349f8f862e3bc6a599da8df39
Maksim Kazyrevich <mkozyrevich@gmail.com>
2019-10-08 00:48:42 +0300

    feat: add task

--- src/index.js
+++ src/index.js
@@ -37,8 +37,55 @@ const MORSE_TABLE = {
     '-----':  '0',
 };
 
-function decode(expr) {
-    // write your solution here
+let decode = function (expr) {
+    let arr = [];
+    let words = '';
+    for (let i=0; i<=(expr.length-10); i=i+10) {
+        arr.push(expr.substr(i, 10));
+    }
+    arr = arr.map(function(item){
+        for (let k=0; k<item.length-1; k=k+2) {
+            if (item.substr(k, 1)!='0') {
+                return item.substr(k);
+            } 
+            else {
+                continue;
+            }
+        }  
+    })
+    
+    arr = arr.map(function(item){
+        let arrElem = [];
+        for (let k=0; k<=(item.length-2); k=k+2) {
+            if (item.substr(k, 2)=='10') {
+                arrElem.push('.');
+            } 
+            else {
+                if (item.substr(k, 2)=='11') {
+                    arrElem.push('-');
+                }
+                else {
+                    arrElem.push(item.substr(k, 2));
+                }
+            }
+        }
+        return arrElem.join('');
+    })
+    for (let item of arr) {
+        for (let key in MORSE_TABLE) {
+            if (item == "**********") {
+                words += ' ';
+                break;
+            }
+            else {
+                if (key == item) {
+                words += MORSE_TABLE[key];
+                }
+        
+            }
+        }
+    }
+    return words;
 }
 
 module.exports = {
